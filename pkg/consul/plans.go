fcc150e32b702cbdb56dc620d45ddd0faae251d4
Nofre <davidprogram417@gmail.com>
2025-03-29 19:03:15 +0100

    added custom grpc resolver and consul watch plans

---
+++ pkg/consul/plans.go
@@ -0,0 +1,63 @@
+package consul
+
+import (
+	"github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/api/watch"
+)
+
+const planType = "service"
+
+type Plan struct {
+	consulURL string
+	service   string
+	*watch.Plan
+	input chan<- []*api.ServiceEntry
+	errCh chan<- error
+}
+
+func NewPlan(consulURL string, serviceName string, input chan<- []*api.ServiceEntry) *Plan {
+	p := &Plan{}
+
+	pl, _ := watch.Parse(map[string]interface{}{
+		"type":        planType,
+		"service":     serviceName,
+		"passingonly": true,
+	})
+
+	p.consulURL = consulURL
+	p.service = serviceName
+	p.Plan = pl
+	p.input = input
+
+	pl.Handler = p.handle
+
+	return p
+}
+
+func (p *Plan) handle(_ uint64, data interface{}) {
+	if !p.IsStopped() {
+		entries := data.([]*api.ServiceEntry)
+		if entries != nil && len(entries) > 0 {
+			p.input <- entries
+		}
+	}
+}
+
+func (p *Plan) Run(errCh chan<- error) {
+	go func() {
+		if err := p.Plan.Run(p.consulURL); err != nil {
+			errCh <- err
+		}
+	}()
+
+	p.errCh = errCh
+}
+
+func (p *Plan) Stop() {
+	p.Plan.Stop()
+
+	if p.Plan.IsStopped() {
+		close(p.input)
+		close(p.errCh)
+	}
+}
